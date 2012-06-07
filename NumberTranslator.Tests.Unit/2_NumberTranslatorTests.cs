d7b7616686e0a070d59ceb639df66a7cf3df15f4
Michael Ibarra <AMER/mibarra@gettyimages.com>
2012-06-07 08:38:03 -0700

    Updated namespaces.

---
+++ NumberTranslator.Tests.Unit/2_NumberTranslatorTests.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+
+namespace GettyImages.Interviews.Tests.Unit
+{
+    [TestFixture]
+    public class NumberTranslatorTests_2
+    {
+        
+        [Test]
+        public void should_translate_11_to_eleven()
+        {
+            Assert.Fail("Test not implemented");
+        }
+
+        [Test]
+        public void should_translate_13_to_thirteen()
+        {
+            Assert.Fail("Test not implemented");
+        }
+
+        [Test]
+        public void should_translate_14_to_fourteen()
+        {
+            Assert.Fail("Test not implemented");
+        }
+
+        [Test]
+        public void should_translate_15_to_fifteen()
+        {
+            Assert.Fail("Test not implemented");
+        }
+
+        [Test]
+        public void should_translate_17_to_seventeen()
+        {
+            Assert.Fail("Test not implemented");
+        }
+    }
+}
---
+++ NumberTranslator.Tests.Unit/3_NumberTranslatorTests.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+
+namespace GettyImages.Interviews.Tests.Unit
+{
+    [TestFixture]
+    public class NumberTranslatorTests_3
+    {
+        [Test]
+        public void should_translate_70_to_seventy()
+        {
+            Assert.Fail("Test not implemented");
+        }
+
+        [Test]
+        public void should_translate_80_to_eighty()
+        {
+            Assert.Fail("Test not implemented");
+        }
+
+        [Test]
+        public void should_translate_90_to_ninety()
+        {
+            Assert.Fail("Test not implemented");
+        }
+
+        [Test]
+        public void should_translate_30_to_thirty()
+        {
+            Assert.Fail("Test not implemented");
+        }
+
+        [Test]
+        public void should_translate_40_to_forty()
+        {
+            Assert.Fail("Test not implemented");
+        }
+    }
+}
